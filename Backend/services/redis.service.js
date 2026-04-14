b707c0c7d6463ef2b2f07d22c3f45b2d24c58068
harrsh45 <harrshpadhiyar@gmail.com>
2026-04-14 11:32:00 +0530

    auth middleware

--- Backend/services/redis.service.js
+++ Backend/services/redis.service.js
@@ -1,12 +1,12 @@
-import Redis from 'ioredis';
-const redis = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_PASSWORD
-}); 
+// import Redis from 'ioredis';
+// const redis = new Redis({
+//     host: process.env.REDIS_HOST,
+//     port: process.env.REDIS_PORT,
+//     password: process.env.REDIS_PASSWORD
+// }); 
 
-redis.on('connect', () => {
-    console.log('Redis connected');
-});
+// redis.on('connect', () => {
+//     console.log('Redis connected');
+// });
 
-export default redis;
+// export default redis;
