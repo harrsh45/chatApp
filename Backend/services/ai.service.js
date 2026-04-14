9a6d156aec0184134087f2ed91350236b7035268
harrsh45 <harrshpadhiyar@gmail.com>
2026-04-14 12:45:54 +0530

    model change

--- Backend/services/ai.service.js
+++ Backend/services/ai.service.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.5-flash",
     generationConfig: {
         responseMimeType: "application/json",
         temperature: 0.4,
